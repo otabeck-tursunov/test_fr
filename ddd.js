80431c778fb68560c75435bc360dbf87949bb2d6
ShaxaKripton <otabecktursunov@gmail.com>
2024-10-11 03:58:50 -0700

    ex

--- ddd.js
+++ ddd.js
@@ -1,21 +1,21 @@
 document.getElementById('checkBalance').addEventListener('click', function() {
    
-    document.getElementById('response').innerText = "Sizning hisobingiz: 0 so'm.";
+    document.getElementById('response').innerText = "Hizmat hali ishga tushirilmadi lekin  ${amount} lekin ushu hizmatlardan @obunachins_bot orqali foydalaning";
 });
 
 document.getElementById('topUpAccount').addEventListener('click', function() {
-    let amount = prompt("@obunachins_bot");
+    let amount = prompt("Hizmat hali ishga tushirilmadi lekin  ${amount} lekin ushu hizmatlardan @obunachins_bot orqali foydalaning");
     if (amount) {
-        document.getElementById('response').innerText = `Hizmat hali ishga tushirilmadi lekin  ${amount} lekin ushu hizmatlardan @obunachins_bot orqali foydalaning`;
+        document.getElementById('response').innerText = `Sizning hisobingiz ${amount} so'mga toʻldirildi.`;
     } else {
-        document.getElementById('response').innerText = "@obunachins_bot";
+        document.getElementById('response').innerText = "Hizmat hali ishga tushirilmadi lekin  ${amount} lekin ushu hizmatlardan @obunachins_bot orqali foydalaning";
     }
 });
 
 document.getElementById('partnership').addEventListener('click', function() {
-    document.getElementById('response').innerText = "Hamkorlik uchun bizga murojaat qiling: @obunachins_bot";
+    document.getElementById('response').innerText = "Hizmat hali ishga tushirilmadi lekin  ${amount} lekin ushu hizmatlardan @obunachins_bot orqali foydalaning";
 });
 
 document.getElementById('help').addEventListener('click', function() {
-    document.getElementById('response').innerText = "Yordam uchun @obunachins_bot ";
+    document.getElementById('response').innerText = "Hizmat hali ishga tushirilmadi lekin  ${amount} lekin ushu hizmatlardan @obunachins_bot orqali foydalaning";
 });
